c117ae9bac3e1c1bcae1ce67bead15786e51de96
dklothe <dklothe@nextility.com>
2016-05-17 14:10:26 -0400

    fix Direct Energy test
    add assertion for last quote, fix bug where last row was skipped

--- test/test_quote_parsers/test_direct_energy.py
+++ test/test_quote_parsers/test_direct_energy.py
@@ -10,19 +10,30 @@ class TestDirectEnergy(QuoteParserTest, TestCase):
 
     # these variables are accessed in QuoteParserTest.setUp
     FILE_NAME = 'Matrix 1 Example - Direct Energy.xls'
-    ALIASES = ['Direct-electric-CT-CLP-37, R35--']
     PARSER_CLASS = DirectEnergyMatrixParser
-    EXPECTED_COUNT = 106554
+    EXPECTED_COUNT = 106560
+
+    def check_every_quote(self, q):
+        self.assertEqual(q.service_type, ELECTRIC)
+        self.assertEqual(q.valid_from, datetime(2015, 5, 4))
+        self.assertEqual(q.valid_until, datetime(2015, 5, 5))
 
     def test_first(self):
         q = self.quotes[0]
+        self.assertEqual(q.term_months, 6)
         self.assertEqual(datetime(2015, 5, 1), q.start_from)
         self.assertEqual(datetime(2015, 6, 1), q.start_until)
-        self.assertEqual(q.term_months, 6)
-        self.assertEqual(q.valid_from, datetime(2015, 5, 4))
-        self.assertEqual(q.valid_until, datetime(2015, 5, 5))
         self.assertEqual(q.min_volume, 0)
         self.assertEqual(q.limit_volume, 75000)
-        self.assertEqual(q.rate_class_alias, self.ALIASES[0])
+        self.assertEqual('Direct-electric-CT-CLP-37, R35--', q.rate_class_alias)
         self.assertEqual(q.price, .07036)
-        self.assertEqual(q.service_type, ELECTRIC)
+
+    def test_last(self):
+        q = self.quotes[-1]
+        self.assertEqual(q.term_months, 25)
+        self.assertEqual(datetime(2016, 4, 1), q.start_from)
+        self.assertEqual(datetime(2016, 5, 1), q.start_until)
+        self.assertEqual(q.min_volume, 750000)
+        self.assertEqual(q.limit_volume, 1e6)
+        self.assertEqual('Direct-electric-RI-NECO-S00--', q.rate_class_alias)
+        self.assertEqual(q.price, .08628)
