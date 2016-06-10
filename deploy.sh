ee4112022dc197605dee97b992608dfccfc60505
dklothe <dklothe@nextility.com>
2016-06-10 15:34:42 -0400

    trying depbug deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -74,6 +74,7 @@ fi
 
 
 echo "Deploying revision ${git_rev} on ${current_branch} ..."
+echo '******************' $git_rev $bitbucket_username $git_repo
 
 cd deployment
 ansible-playbook -i $1 app.yml --extra-vars "revision=${git_rev} hg_username=${bitbucket_username} hg_password=${password} hg_branch=${current_branch} hg_repo=${git_repo}"
