2449836eeed88069cbbac8c794b48ef6d77e5432
Raza Gilani <rgilani@skylineinnovations.com>
2016-10-03 13:10:13 -0400

    updated the parser for source energy

--- test/test_quote_parsers/test_source.py
+++ test/test_quote_parsers/test_source.py
@@ -9,9 +9,9 @@ from test.test_quote_parsers import QuoteParserTest
 class TestSource(QuoteParserTest, TestCase):
 
     # these variables are accessed in QuoteParserTest.setUp
-    FILE_NAME = 'Source 20160314_SPG PJM Small Comm Matrix Table.zip'
+    FILE_NAME = '20161003_SPG PJM Small Comm Matrix Table.zip'
     PARSER_CLASS = SourceMatrixParser
-    EXPECTED_COUNT = 731640
+    EXPECTED_COUNT = 762048
 
     @classmethod
     def get_quote_list(cls):
@@ -22,29 +22,29 @@ class TestSource(QuoteParserTest, TestCase):
 
     def test_first(self):
         q = self.quotes[0]
-        self.assertEqual(datetime(2016, 3, 1), q.start_from)
-        self.assertEqual(datetime(2016, 4, 1), q.start_until)
+        self.assertEqual(datetime(2016, 10, 1), q.start_from)
+        self.assertEqual(datetime(2016, 11, 1), q.start_until)
         self.assertEqual(1, q.term_months)
-        self.assertEqual(datetime(2016, 3, 14), q.valid_from)
-        self.assertEqual(datetime(2016, 3, 15), q.valid_until)
+        self.assertEqual(datetime(2016, 10, 1), q.valid_from)
+        self.assertEqual(datetime(2016, 10, 2), q.valid_until)
         self.assertEqual(0, q.min_volume)
         self.assertEqual(25000, q.limit_volume)
         self.assertEqual('Source-electric-PEPCO DC-Rate GS 3A',
                          q.rate_class_alias)
-        self.assertEqual(.05103, q.price)
+        self.assertEqual(0.10288, q.price)
         self.assertEqual(q.service_type, ELECTRIC)
 
     def test_last(self):
         q = self.quotes[-1]
-        self.assertEqual(datetime(2017, 2, 1), q.start_from)
-        self.assertEqual(datetime(2017, 3, 1), q.start_until)
-        self.assertEqual(27, q.term_months)
-        self.assertEqual(datetime(2016, 3, 14), q.valid_from)
-        self.assertEqual(datetime(2016, 3, 15), q.valid_until)
+        self.assertEqual(datetime(2017, 9, 1), q.start_from)
+        self.assertEqual(datetime(2017, 10, 1), q.start_until)
+        self.assertEqual(36, q.term_months)
+        self.assertEqual(datetime(2016, 10, 1), q.valid_from)
+        self.assertEqual(datetime(2016, 10, 2), q.valid_until)
         self.assertEqual(900000, q.min_volume)
         self.assertEqual(1000000, q.limit_volume)
         self.assertEqual(
             ('Source-electric-Atlantic City Electric-Rate SPL\n'
             'Street and Private Lighting'), q.rate_class_alias)
-        self.assertEqual(0.08955, q.price)
+        self.assertEqual(0.10041, q.price)
         self.assertEqual(q.service_type, ELECTRIC)
